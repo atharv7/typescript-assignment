5b617584912973fb79259e5b506521e1cdacad23
atharv7 <virtuarchitects7@gmail.com>
2018-04-22 13:34:58 +0200

    Color check added on game change

--- src/games/controller.ts
+++ src/games/controller.ts
@@ -1,6 +1,6 @@
-import { JsonController, Get,Put,Body, Param,Post,HttpCode } from 'routing-controllers'
+import { JsonController, Get,Put,Body, Param,Post,HttpCode,NotFoundError } from 'routing-controllers'
 import Game from './entity'
-type GameList = { games: Game[] }
+
 
 
 @JsonController()
@@ -20,20 +20,29 @@ export default class GameController {
         }
 
     @Put('/games/:id')
-    updatePage(
-    @Param('id') id: number,
-    @Body() body: Partial<Game>
-    ): Game { 
-        console.log(`Incoming PUT body param:`, body)
-    return Game.findOne[id]
-    }
+    async updateGame(
+        @Param('id') id: number,
+        @Body() update: Partial<Game>
+    ) {
+        const game = await Game.findOne(id)
+        if (!game) throw new NotFoundError('Invalid game ID')
+        if(update.color && !(update.color==='red'||
+            update.color==='blue'||
+            update.color==='green'||
+            update.color==='yellow'||
+            update.color==='magenta')) 
+            {
+                return 'Invalid Color!'
+            }
+                return Game.merge(game, update).save()
 
+    }
+    
     @Post('/games')
     @HttpCode(201)
     createGame(
-    @Body() body: Game
-    ): Game {
-    console.log(`Incoming POST body param:`, body)
-    return body
-}   
+    @Body() game: Game
+    ) {
+        return game.save()
+    }
 }
--- target/games/data.js
+++ target/games/data.js
@@ -1,23 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const defaultBoard = [['o', 'o', 'o'], ['o', 'o', 'o'], ['o', 'o', 'o']];
 const gamesById = {
     1: {
         id: 1,
         name: 'first game',
         color: 'green',
-        board: [['o', 'o', 'o'], ['o', 'o', 'o'], ['o', 'o', 'o']]
+        board: defaultBoard
     },
     2: {
         id: 1,
         name: 'second game',
         color: 'blue',
-        board: [['o', 'o', 'o'], ['o', 'o', 'o'], ['o', 'o', 'o']]
+        board: defaultBoard
     },
     3: {
         id: 1,
         name: 'third game',
         color: 'yellow',
-        board: [['o', 'o', 'o'], ['o', 'o', 'o'], ['o', 'o', 'o']]
+        board: defaultBoard
     }
 };
 exports.default = gamesById;
